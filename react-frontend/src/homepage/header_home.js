fd9b93e52c7029ca33deb904a088a1edbded770d
Lan Nguyen <lannguyen@Lans-MBP.lan>
2022-05-01 00:13:15 -0700

    right align menu, name, avatar

--- react-frontend/src/homepage/header_home.js
+++ react-frontend/src/homepage/header_home.js
@@ -8,21 +8,22 @@ function HeaderHome(props)
         <div className="header_home">
             <h1 className="header_title">Dialed In</h1>
 
-            <img class="user_avatar" src="https://yt3.ggpht.com/ytc/AKedOLTjWeLVEfOwpOW2VApxNxs6FW1Z0WbvQly_TruQjA=s900-c-k-c0x00ffffff-no-rj" alt=""/>
+            <div className="userNMenu">
+                <img class="user_avatar" src="https://yt3.ggpht.com/ytc/AKedOLTjWeLVEfOwpOW2VApxNxs6FW1Z0WbvQly_TruQjA=s900-c-k-c0x00ffffff-no-rj" alt=""/>
 
+                <div className="user_name"> John Doe </div>
 
-            <div className="user_name"> John Doe </div>
+                <div class="dropdown">
+                    <button class="btn btn-secondary" type="button" 
+                            id="dropdownMenuButton" data-bs-toggle="dropdown" 
+                            aria-haspopup="true" aria-expanded="false">
+                    </button>
 
-            <div class="dropdown">
-                <button class="btn btn-secondary" type="button" 
-                    id="dropdownMenuButton" data-bs-toggle="dropdown" 
-                    aria-haspopup="true" aria-expanded="false">
-                </button>
-
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
                 </div>
             </div>
         </div>

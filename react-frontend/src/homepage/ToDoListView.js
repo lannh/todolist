227e8061cb4449b6a15f27eaae3e37fd5260bc5f
logan <loganreismarkson@live.com>
2022-05-27 20:03:41 -0700

    various changes to how tasks are displayed from the database, work has been done to display time correctly as well as show individual task intervals. tasks also display priority colors now depending on the max and min priority of the day

--- react-frontend/src/homepage/ToDoListView.js
+++ react-frontend/src/homepage/ToDoListView.js
@@ -16,16 +16,48 @@ import * as scheduler from "../scheduler.js";
 /* Max task_name = 20 chars*/
 
 //convert start time to string
+
+// function tts(t)
+// {
+// 	var hour = Math.floor(t);
+// 	var min = Math.floor((t - hour) * 60);
+// 	if (hour < 1)
+// 		return "0:" + min.toString();
+
+// 	var min_string = "0:";
+// 	if (min < 10)
+// 		min_string = ":0";
+// 	min_string += min.toString();
+
+// 	return hour.toString() + min_string;
+// }
+
 function csts(t)
 {
 	// t = t / (24 * 60); for later
-	if(t > 12)
-		return (t - 12) + "PM";
+	var hour = Math.floor(t);
+	var min = Math.floor((t - hour) * 60);
 
-	if(t == 0)
-		t = 12;
+	var am_pm = "AM";
 
-	return t + "AM";
+	if(hour > 12)
+	{
+		hour = hour - 12;
+		am_pm = "PM";
+	}
+	if (hour == 12)
+		am_pm = "PM";
+
+	if(hour == 0)
+		hour = 12;
+	var hour_string = hour.toString();
+
+	var min_string = ":";
+	if (min < 10)
+		min_string = ":0";
+	min_string += min.toString();
+
+	return hour_string + min_string + am_pm;
 }
 
 //get block string
@@ -35,6 +67,52 @@ function gbs(bd)
 	var e = csts(bd.end_time);
 
 	return s + " - " + e;
+}
+
+function get_min_max_priority(task_data)
+{
+	var min = -1;
+	var max = -1;
+	for (let task_index = 0; 
+		task_index < task_data.length; 
+		task_index++)
+	{
+		var task = task_data[task_index];
+		if (min == -1 || task.priority < min)
+			min = task.priority;
+
+		if (max == -1 || task.priority > max)
+			max = task.priority;
+	}
+	return [min, max];
+}
+
+function prio_color_scale(p, min, max)
+{
+	var v = min;
+	if(min != max)
+		v = (p - min) / (max - min);
+	var color_a = rgb_to_hex(237, 88, 88);
+	var color_b = rgb_to_hex(115, 237, 88);
+	return lerp_hex_colors(color_a, color_b, v);
+}
+
+function rgb_to_hex(r, g, b) //yoink
+{
+	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function lerp_hex_colors(a, b, amount) //yoink
+{ 
+	var ah = parseInt(a.replace(/#/g, ""), 16),
+		ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
+		bh = parseInt(b.replace(/#/g, ""), 16),
+		br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
+		rr = ar + amount * (br - ar),
+		rg = ag + amount * (bg - ag),
+		rb = ab + amount * (bb - ab);
+
+	return "#" + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
 }
 
 function ToDoListView() 
@@ -63,16 +141,29 @@ function ToDoListView()
 			setTasks(result);
 	});
 
-	for (let task_index = 0; 
-		task_index < tasks.length; 
-		task_index++)
+	var start = new Date(Date.now());
+	const current_day = start.getDate();
+	const current_month = start.getMonth();
+	console.log(current_day, current_month);
+
+
+	for (let task_index = tasks.length-1; 
+		task_index >= 0; 
+		task_index--)
 	{
-		tasks[task_index].length = 1;
-		tasks[task_index].priority = 1;
+		// const task_month = tasks[task_index].due_date.getMonth();
+		// const task_day = tasks[task_index].due_date.getDate();
+		// if(!(task_month == current_month && current_day == task_day))
+		// {
+		// 	// tasks.splice(task_index, 1);
+		// }
+		tasks[task_index].length = 0.5 + task_index / 5;
+		tasks[task_index].priority = task_index;
 	}
 	var schedule_data = scheduler.solve_schedule(tasks,
 		scheduler.debug_schedule_blocks);
 
+	var thresh = get_min_max_priority(tasks);
 	return (
 		<div className="d-flex flex-column" id="todolist_col">
 			<div className="p-2" id="todolist_title">
@@ -80,7 +171,7 @@ function ToDoListView()
 			</div>
 
 			{schedule_data.map((schedule_block) => (
-				console.log(schedule_block),
+				console.log(""),
 				
 				<div className="p-2" id="todo_list">
 					<span style={{ color: "white", fontSize: 32}}>
@@ -89,15 +180,21 @@ function ToDoListView()
 					
 					{schedule_block.activities.map((task_data) => (
 						
-						console.log(schedule_block),
+						console.log(""),
 						
 						<div className="card" id="todo_item">
 							<div className="card-body">
 								<div className="row row-2" id="task_info">
 									<div className="col-sm-auto" 
 										id="time_task">
-										{task_data.length + " hr"}
+										{csts(task_data.start_time) + "-" 
+										+ csts(task_data.start_time + task_data.length)}
 									</div>
+
+									{/* <div className="col-sm-auto" 
+										id="time_task">
+										{"Length[ "+tts(task_data.length)+" ]"}
+									</div> */}
 
 									<div className="col col-sm-fill" 
 										id="task_name">
@@ -106,11 +203,14 @@ function ToDoListView()
 
 									<div className="col-sm-auto" 
 										id="priority_level">
-										<i className="bi bi-star-fill"
-											id="high_priority" 
-											data-toggle="tooltip"
-											data-placement="auto"
-											title="Priority: High" />
+										<i style={{color: prio_color_scale(task_data.priority, 
+											thresh[0], 
+											thresh[1])}}
+										className="bi bi-star-fill"
+										id="high_priority" 
+										data-toggle="tooltip"
+										data-placement="auto"
+										title="Priority: High" />
 									</div>
 
 									<div className="col-sm-auto" 

78a1b0ac98315242fcc25d679cd3c4f7b01d5eee
Nathan Tran <90416892+na10tran@users.noreply.github.com>
2022-05-31 13:18:16 -0700

    fixing todolist

--- react-frontend/src/homepage/ToDoListView.js
+++ react-frontend/src/homepage/ToDoListView.js
@@ -61,7 +61,7 @@ function csts(t)
 }
 
 //get block string
-/*function gbs(bd)
+function gbs(bd)
 {
 	var s = csts(bd.start_time);
 	var e = csts(bd.end_time);
@@ -258,24 +258,6 @@ function ToDoListView()
 					))}
 				</div >
 			))}
-		</div >
-	);
-}*/
-function ToDoListView(props) 
-{
-	const tasks = props.characterData.map((task_data,index) =>
-		<div key={index}> 
-			<div> {task_data.taskName} </div>
-		</div>
-	);
-	return (
-		<div className="d-flex flex-column" id="todolist_col">
-			<div className="p-2" id="todolist_title">
-				<span>To-Do</span>
-			</div>
-			<div className="p-2" id="todo_list">
-				{tasks}
-			</div >
 		</div >
 	);
 }
